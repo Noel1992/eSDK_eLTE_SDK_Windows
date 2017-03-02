7feeaaec7d11864c6e65093adee97b47e401cc6e
SWei1234 <songwei12@huawei.com>
2017-03-02 15:52:38 +0800

    【需求or问题单号】无 【合入版本】eSDK_eLTE_V200R001C10 【问题描述】更新vlc串流文件 【修改说明】更新vlc串流文件

--- sample/simplest_libvlc_streamer.cpp
+++ sample/simplest_libvlc_streamer.cpp
@@ -1,1 +1,34 @@
-":sout-keep", ":no-sout-standard-sap", ":no-sout-rtp-sap"
+#include <Windows.h>
+#include "vlc/vlc.h"
+
+int main(int argc, char **argv)
+{
+	libvlc_instance_t * inst;
+	inst = libvlc_new (0, NULL);
+
+	libvlc_media_player_t *mp1;
+	libvlc_media_t *m1;
+
+	m1 = libvlc_media_new_location(inst, "file:///D:\\test.sdp");
+
+	const char *options1[] = {
+		":no-audio",
+		":sout=#transcode{vcodec=theo,vb=1024,fps=25,acodec=none}:http{mux=ogg,dst=:8080/xxzy}",
+		":network-caching=10",
+		":netsync-master"
+	};
+
+	for (int i = 0; i < sizeof(options1) / sizeof(options1[0]); i++)
+	{
+		libvlc_media_add_option (m1, options1[i]);
+	}
+	mp1 = libvlc_media_player_new_from_media (m1);
+	libvlc_media_release (m1);
+	libvlc_media_player_play (mp1);
+
+	while(true)
+	{
+		;
+	}
+	return 0;
+}
